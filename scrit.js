a0511ae3cef2d5e48abec76ec67df8fc5cb8a3dd
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-10-15 00:02:45 +0530

    Update scrit.js

--- scrit.js
+++ scrit.js
@@ -1,1 +1,13 @@
+const text="Talk is cheep, show me the code";
 
+let index =0;
+
+function writeText(){
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+    if(index > text.length){
+        index=0
+    }
+}
+setInterval(writeText, 100)
